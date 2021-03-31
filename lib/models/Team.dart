5c9a4e03088a986ec8dceedf6498541c9e8ba230
Benjamin <ben88200@gmail.com>
2021-03-31 20:58:58 +0200

    Ajout classe Team

--- lib/models/Team.dart
+++ lib/models/Team.dart
@@ -1,10 +1,15 @@
 import 'package:the_battle/models/Character.dart';
 import 'package:the_battle/models/Player.dart';
+import 'package:the_battle/data/characters.dart' as staticData;
+import 'dart:math' as math;
 
 class Team {
   final String uuid;
+  String name = "My Team";
   final Player player;
   List<Character> characters;
 
-  Team(this.uuid, this.player);
+  var randomNumber = new math.Random();
+
+  Team(this.uuid, this.name, this.player);
 }
